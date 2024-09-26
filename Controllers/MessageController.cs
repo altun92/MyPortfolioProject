707b1e53ad16e888ff408503b3c4c6a713ad832f
AnıL .. <azrail1626@hotmail.com>
2024-09-26 21:53:34 +0300

    ...

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MyPortfolioProject.Models;
 
 namespace MyPortfolioProject.Controllers
 {
     public class MessageController : Controller
     {
+        DbMyPortfolioEntities context = new DbMyPortfolioEntities();
         // GET: Message
         public ActionResult Inbox()
         {
-            return View();
+            var value = context.Contact.ToList();
+            return View(value);
+        }
+
+        public ActionResult ChangeMessageStatusToTrue(int id)
+        {
+            var value = context.Contact.Find(id);
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+
+        public ActionResult ChangeMessageStatusToFalse(int id)
+        {
+            var value = context.Contact.Find(id);
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
         }
     }
 }
